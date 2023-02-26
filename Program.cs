a61377a14001aa587bca88eb6e912424a7598b4b
Feshkineugen <118047190+Feshkineugen@users.noreply.github.com>
2023-02-26 17:44:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Random random= new Random();
+int[] array = new int[1000];
+for (int i = 0;i< array.Length; i++)
+    array[i] = random.Next(1,100);
+//создание рандомного массива
+Array.Sort(array);/*сортировка*/
+int povMajor = -1, major = 0, tecPovMajor = 0, tecMajor = array[0];
+//обЪявление переменных
+foreach(int i in array)
+{
+    if (i == tecMajor)
+    {
+        tecPovMajor ++;
+    }
+    
+    else
+    {
+        tecPovMajor = 1;
+        tecMajor = i;
+    }
+    if(tecPovMajor > povMajor)
+    {
+        povMajor = tecPovMajor;
+        major = i;
+    }
+    Console.WriteLine($"Число которое больше всего повторяется: "+ major +".Количество его повтора:" +povMajor  );
+}
